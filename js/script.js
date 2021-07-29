6334f7532b71aacb0533a231ea386cb8c8ed4efc
Evgeniy_Zozulya <zeawork@gmail.com>
2021-07-29 13:28:03 +0300

    Добавлены страницы регистрации, sign in, password

--- js/script.js
+++ js/script.js
@@ -21,6 +21,33 @@ jQuery(document).ready(function () {
   godefer.parentNode.insertBefore(giftofspeed, godefer);
   
   
+  $('#phone').each(function(){
+    var input = document.querySelector("#phone");
+    window.intlTelInput(input, {
+      // allowDropdown: false,
+      // autoHideDialCode: false,
+      // autoPlaceholder: "off",
+      // dropdownContainer: document.body,
+      // excludeCountries: ["us"],
+      // formatOnDisplay: false,
+      // geoIpLookup: function(callback) {
+      //   $.get("http://ipinfo.io", function() {}, "jsonp").always(function(resp) {
+      //     var countryCode = (resp && resp.country) ? resp.country : "";
+      //     callback(countryCode);
+      //   });
+      // },
+      // hiddenInput: "full_number",
+      // initialCountry: "auto",
+      // localizedCountries: { 'de': 'Deutschland' },
+      // nationalMode: false,
+      // onlyCountries: ['us', 'gb', 'ch', 'ca', 'do'],
+      // placeholderNumberType: "MOBILE",
+      // preferredCountries: ['cn', 'jp'],
+      // separateDialCode: true,
+      //utilsScript: "build/js/utils.js",
+    });
+  });
+  
   AOS.init();
   
   $("video").prop("volume", 0.3).click(function(){this[this.paused?"play":"pause"]()});
