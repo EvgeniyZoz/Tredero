292fe563e0dc15b5b3e7752cdca35fb51c5c51f1
Evgeniy_Zozulya <zeawork@gmail.com>
2021-08-05 18:16:36 +0300

    Добавил остальные страницы на проверку

--- js/script.js
+++ js/script.js
@@ -270,206 +270,69 @@ jQuery(document).ready(function () {
     } , 2000);
   }
 
-    
-    
+  $('.input__ranged').each(function(){
+    $(this).inputNumber({
+      negative:false, //allow negative numbers
+      positive:true, //allow positive numbers
+      wrapClass:'ranged__input',
+      upClass:'up',
+      upTitle:'Incrase',
+      downClass:'down',
+      downTitle:'Decrace'
+    });
+  });
   
-    
 
+  var swiper1 = new Swiper(".blogSwiper", {
+    lazy: true,
+     slidesPerView: 1,
+    spaceBetween: 30,
+    //autoHeight: true,
+    navigation: {
+      nextEl: ".blog__next",
+      prevEl: ".blog__prev",
+    },
+    breakpoints: {
+      220:{
+        slidesPerView: 1,
+        slidesPerColumn:3,        
+        Autoheight:true
+      },
+      640: {
+        slidesPerView: 2,
+        slidesPerColumn:2,
+      },
+      1198: {
+        slidesPerView: 3,
+      },
+      1799: {
+        slidesPerView: 4
+      },
+    }
+  });
+  
+  /*tabs*/
+  $('.tabs li.active a').each(function(){
+    var tabId = $(this).attr('href')+'_';
+    $(tabId).addClass("activated");
+  })
 
-  
-  
+  $('.tabs li').click(function(e){
+    var menuActive = $(this).find('a').attr('href')+'_';
+    menuActive = menuActive.substr(menuActive.indexOf('#'));
+    $(this).parents('.tabs__wrapper').find('.tabs__box').removeClass('activated')
+    $(this).parents('.tabs__wrapper').find('.tabs li').removeClass('active');
+    $(this).addClass('active');
+    $(menuActive).addClass('activated')
+    if (!$(this).parents('.tabs').hasClass('tabs__links')){
+        e.preventDefault();
+    }
+  }) 
+
   
 });
 
 
-/*------------------ANIM---------------------------*/
-
-// if ($(window).innerWidth() > 1024) {
-
-
-/*------------------ANIM---------------------------*/
-
-
-
-
-
-
-/*------------------ANIM---------------------------*/
-/*$(document).on('mouseenter', '.anim-item', function (event) {
-
-
-  const $circle1 = this.querySelector('.card__circle');
-
-
-
-  const $smallCircle1 = this.querySelector('.card__smallCircle');
-
-
-  const $year1 = this.querySelector('.card__year');
-
-
-
-  const $card1 = this.querySelector('.card');
-
-
-  const $cardOrangeShine1 = this.querySelector('.card__orangeShine');
-
-  const $cardComet1 = this.querySelector('.card__cometOuter');
-  const $cardComet2 = this.querySelector('.card__cometOuter2');
-  const $cardComet3 = this.querySelector('.card__cometOuter3');
-  const $cardComet4 = this.querySelector('.card__cometOuter4');
-  const $cardComet5 = this.querySelector('.card__cometOuter5');
-
-
-  // const $circle1 = document.querySelector('.anim-item-1 .card__circle');
-  // const $circle2 = document.querySelector('.anim-item-2 .card__circle');
-  // const $circle3 = document.querySelector('.anim-item-3 .card__circle');
-  // const $circle4 = document.querySelector('.anim-item-4 .card__circle');
-  //
-  //
-  // const $smallCircle1 = document.querySelector('.anim-item-1 .card__smallCircle');
-  // const $smallCircle2 = document.querySelector('.anim-item-2 .card__smallCircle');
-  // const $smallCircle3 = document.querySelector('.anim-item-3 .card__smallCircle');
-  // const $smallCircle4 = document.querySelector('.anim-item-4 .card__smallCircle');
-  //
-  //
-  //
-  // const $year1 = document.querySelector('.anim-item-1 .card__year');
-  // const $year2 = document.querySelector('.anim-item-2 .card__year');
-  // const $year3 = document.querySelector('.anim-item-3 .card__year');
-  // const $year4 = document.querySelector('.anim-item-4 .card__year');
-  //
-  //
-  // const $card1 = document.querySelector('.anim-item-1 .card');
-  // const $card2 = document.querySelector('.anim-item-2 .card');
-  // const $card3 = document.querySelector('.anim-item-3 .card');
-  // const $card4 = document.querySelector('.anim-item-4 .card');
-  //
-  //
-  // const $cardOrangeShine1 = document.querySelector('.anim-item-1 .card__orangeShine');
-  // const $cardOrangeShine2 = document.querySelector('.anim-item-2 .card__orangeShine');
-  // const $cardOrangeShine3 = document.querySelector('.anim-item-3 .card__orangeShine');
-  // const $cardOrangeShine4 = document.querySelector('.anim-item-4 .card__orangeShine');
-  //
-  //
-  //
-  // const $cardComet1 = document.querySelector('.anim-item-1 .card__cometOuter');
-  // const $cardComet2 = document.querySelector('.anim-item-2 .card__cometOuter');
-  // const $cardComet3 = document.querySelector('.anim-item-3 .card__cometOuter');
-  // const $cardComet4 = document.querySelector('.anim-item-4 .card__cometOuter');
-
-  const generateTranslate = (el, e, value) => {
-    console.log(e)
-
-    el.style.transform = `translate(${e.clientX * value}px, ${e.clientY * value}px)`;
-  }
-  const cumulativeOffset = (element) => {
-    var top = 0, left = 0;
-    do {
-      top += element.offsetTop || 0;
-      left += element.offsetLeft || 0;
-      element = element.offsetParent;
-    } while (element);
-
-    return {
-      top: top,
-      left: left
-    };
-  };
-
-
-
-  document.onmousemove = (event) => {
-
-
-
-    const e = event || window.event;
-    const x1 = (e.pageX - cumulativeOffset($card1).left - (350 / 2)) * -1 / 100;
-    // const x2 = (e.pageX - cumulativeOffset($card2).left - (350 / 2)) * -1 / 100;
-    // const x3 = (e.pageX - cumulativeOffset($card3).left - (350 / 2)) * -1 / 100;
-    // const x4 = (e.pageX - cumulativeOffset($card4).left - (350 / 2)) * -1 / 100;
-
-    const y1 = (e.pageY - cumulativeOffset($card1).top - (350 / 2)) * -1 / 100;
-    // const y2 = (e.pageY - cumulativeOffset($card2).top - (350 / 2)) * -1 / 100;
-    // const y3 = (e.pageY - cumulativeOffset($card3).top - (350 / 2)) * -1 / 100;
-    // const y4 = (e.pageY - cumulativeOffset($card4).top - (350 / 2)) * -1 / 100;
-
-    const matrix1 = [
-      [1, 0, 0, -x1 * 0.00005],
-      [0, 1, 0, -y1 * 0.00005],
-      [0, 0, 1, 1],
-      [0, 0, 0, 1]
-    ];
-
-    // const matrix2 = [
-    //   [1, 0, 0, -x2 * 0.00005],
-    //   [0, 1, 0, -y2 * 0.00005],
-    //   [0, 0, 1, 1],
-    //   [0, 0, 0, 1]
-    // ];
-    //
-    // const matrix3 = [
-    //   [1, 0, 0, -x3 * 0.00005],
-    //   [0, 1, 0, -y3 * 0.00005],
-    //   [0, 0, 1, 1],
-    //   [0, 0, 0, 1]
-    // ];
-    //
-    // const matrix4 = [
-    //   [1, 0, 0, -x4 * 0.00005],
-    //   [0, 1, 0, -y4 * 0.00005],
-    //   [0, 0, 1, 1],
-    //   [0, 0, 0, 1]
-    // ];
-    generateTranslate($circle1, e, 0.05);
-    // generateTranslate($circle2, e, 0.05);
-    // generateTranslate($circle3, e, 0.05);
-    // generateTranslate($circle4, e, 0.05);
-
-
-
-    generateTranslate($smallCircle1, e, 0.03);
-    // generateTranslate($smallCircle2, e, 0.03);
-    // generateTranslate($smallCircle3, e, 0.03);
-    // generateTranslate($smallCircle4, e, 0.03);
-
-
-    generateTranslate($year1, e, 0.03);
-    // generateTranslate($year2, e, 0.03);
-    // generateTranslate($year3, e, 0.03);
-    // generateTranslate($year4, e, 0.03);
-
-
-    generateTranslate($cardOrangeShine1, e, 0.09);
-    // generateTranslate($cardOrangeShine2, e, 0.09);
-    // generateTranslate($cardOrangeShine3, e, 0.09);
-    // generateTranslate($cardOrangeShine4, e, 0.09);
-
-    generateTranslate($cardComet1, e, 0.05);
-    generateTranslate($cardComet2, e, 0.05);
-    generateTranslate($cardComet3, e, 0.05);
-    generateTranslate($cardComet4, e, 0.05);
-    generateTranslate($cardComet5, e, 0.05);
-    // generateTranslate($cardComet2, e, 0.05);
-    // generateTranslate($cardComet3, e, 0.05);
-    // generateTranslate($cardComet4, e, 0.05);
-
-    $card1.style.transform = `matrix3d(${matrix1.toString()})`;
-    // $card2.style.transform = `matrix3d(${matrix2.toString()})`;
-    // $card3.style.transform = `matrix3d(${matrix3.toString()})`;
-    // $card4.style.transform = `matrix3d(${matrix4.toString()})`;
-  }
-});
-
-$(document).on('mouseover', '.anim-item', function (e) {
-  $(this).addClass('is-active');
-  setTimeout(function () {
-    $('.anim-item').removeClass('is-active');
-  }, 200);
-});
-$(document).on('mouseleave', '.anim-item', function (event) {
-
-});*/
 
 /*-----------------END-ANIM---------------------------*/
 
